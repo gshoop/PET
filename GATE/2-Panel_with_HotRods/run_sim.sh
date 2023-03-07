6b4250849997d460ebf6bdf0dd824414607e3e22
grshoop <greysonshoop@gmail.com>
2023-03-06 16:55:54 -0800

    Added code from greg

---
+++ GATE/2-Panel_with_HotRods/run_sim.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+(
+(
+mkdir ./output/out_0 &
+mkdir ./output/out_1 &
+mkdir ./output/out_2 &
+mkdir ./output/out_3 &
+mkdir ./output/out_4 &
+mkdir ./output/out_5 &
+mkdir ./output/out_6 &
+mkdir ./output/out_7 &
+wait
+Gate -a [outputfolder,./output/out_0][timeslice,1.0][timestart,0.0][timestop,1.0] main.mac > ./log/log_0.txt &
+Gate -a [outputfolder,./output/out_1][timeslice,1.0][timestart,1.0][timestop,2.0] main.mac > ./log/log_1.txt &
+Gate -a [outputfolder,./output/out_2][timeslice,1.0][timestart,2.0][timestop,3.0] main.mac > ./log/log_2.txt &
+Gate -a [outputfolder,./output/out_3][timeslice,1.0][timestart,3.0][timestop,4.0] main.mac > ./log/log_3.txt &
+Gate -a [outputfolder,./output/out_4][timeslice,1.0][timestart,4.0][timestop,5.0] main.mac > ./log/log_4.txt &
+Gate -a [outputfolder,./output/out_5][timeslice,1.0][timestart,5.0][timestop,6.0] main.mac > ./log/log_5.txt &
+Gate -a [outputfolder,./output/out_6][timeslice,1.0][timestart,6.0][timestop,7.0] main.mac > ./log/log_6.txt &
+Gate -a [outputfolder,./output/out_7][timeslice,1.0][timestart,7.0][timestop,8.0] main.mac > ./log/log_7.txt &
+wait
+mkdir ./output/out_8 &
+mkdir ./output/out_9 &
+mkdir ./output/out_10 &
+mkdir ./output/out_11 &
+mkdir ./output/out_12 &
+mkdir ./output/out_13 &
+mkdir ./output/out_14 &
+mkdir ./output/out_15 &
+wait
+Gate -a [outputfolder,./output/out_8][timeslice,1.0][timestart,8.0][timestop,9.0] main.mac > ./log/log_8.txt &
+Gate -a [outputfolder,./output/out_9][timeslice,1.0][timestart,9.0][timestop,10.0] main.mac > ./log/log_9.txt &
+Gate -a [outputfolder,./output/out_10][timeslice,1.0][timestart,10.0][timestop,11.0] main.mac > ./log/log_10.txt &
+Gate -a [outputfolder,./output/out_11][timeslice,1.0][timestart,11.0][timestop,12.0] main.mac > ./log/log_11.txt &
+Gate -a [outputfolder,./output/out_12][timeslice,1.0][timestart,12.0][timestop,13.0] main.mac > ./log/log_12.txt &
+Gate -a [outputfolder,./output/out_13][timeslice,1.0][timestart,13.0][timestop,14.0] main.mac > ./log/log_13.txt &
+Gate -a [outputfolder,./output/out_14][timeslice,1.0][timestart,14.0][timestop,15.0] main.mac > ./log/log_14.txt &
+Gate -a [outputfolder,./output/out_15][timeslice,1.0][timestart,15.0][timestop,16.0] main.mac > ./log/log_15.txt &
+wait
+mkdir ./output/out_16 &
+mkdir ./output/out_17 &
+mkdir ./output/out_18 &
+mkdir ./output/out_19 &
+mkdir ./output/out_20 &
+mkdir ./output/out_21 &
+mkdir ./output/out_22 &
+mkdir ./output/out_23 &
+wait
+Gate -a [outputfolder,./output/out_16][timeslice,1.0][timestart,16.0][timestop,17.0] main.mac > ./log/log_16.txt &
+Gate -a [outputfolder,./output/out_17][timeslice,1.0][timestart,17.0][timestop,18.0] main.mac > ./log/log_17.txt &
+Gate -a [outputfolder,./output/out_18][timeslice,1.0][timestart,18.0][timestop,19.0] main.mac > ./log/log_18.txt &
+Gate -a [outputfolder,./output/out_19][timeslice,1.0][timestart,19.0][timestop,20.0] main.mac > ./log/log_19.txt &
+Gate -a [outputfolder,./output/out_20][timeslice,1.0][timestart,20.0][timestop,21.0] main.mac > ./log/log_20.txt &
+Gate -a [outputfolder,./output/out_21][timeslice,1.0][timestart,21.0][timestop,22.0] main.mac > ./log/log_21.txt &
+Gate -a [outputfolder,./output/out_22][timeslice,1.0][timestart,22.0][timestop,23.0] main.mac > ./log/log_22.txt &
+Gate -a [outputfolder,./output/out_23][timeslice,1.0][timestart,23.0][timestop,24.0] main.mac > ./log/log_23.txt &
+wait
+mkdir ./output/out_24 &
+mkdir ./output/out_25 &
+mkdir ./output/out_26 &
+mkdir ./output/out_27 &
+mkdir ./output/out_28 &
+mkdir ./output/out_29 &
+mkdir ./output/out_30 &
+mkdir ./output/out_31 &
+wait
+Gate -a [outputfolder,./output/out_24][timeslice,1.0][timestart,24.0][timestop,25.0] main.mac > ./log/log_24.txt &
+Gate -a [outputfolder,./output/out_25][timeslice,1.0][timestart,25.0][timestop,26.0] main.mac > ./log/log_25.txt &
+Gate -a [outputfolder,./output/out_26][timeslice,1.0][timestart,26.0][timestop,27.0] main.mac > ./log/log_26.txt &
+Gate -a [outputfolder,./output/out_27][timeslice,1.0][timestart,27.0][timestop,28.0] main.mac > ./log/log_27.txt &
+Gate -a [outputfolder,./output/out_28][timeslice,1.0][timestart,28.0][timestop,29.0] main.mac > ./log/log_28.txt &
+Gate -a [outputfolder,./output/out_29][timeslice,1.0][timestart,29.0][timestop,30.0] main.mac > ./log/log_29.txt &
+Gate -a [outputfolder,./output/out_30][timeslice,1.0][timestart,30.0][timestop,31.0] main.mac > ./log/log_30.txt &
+Gate -a [outputfolder,./output/out_31][timeslice,1.0][timestart,31.0][timestop,32.0] main.mac > ./log/log_31.txt &
+wait
+mkdir ./output/out_32 &
+mkdir ./output/out_33 &
+mkdir ./output/out_34 &
+mkdir ./output/out_35 &
+mkdir ./output/out_36 &
+mkdir ./output/out_37 &
+mkdir ./output/out_38 &
+mkdir ./output/out_39 &
+wait
+Gate -a [outputfolder,./output/out_32][timeslice,1.0][timestart,32.0][timestop,33.0] main.mac > ./log/log_32.txt &
+Gate -a [outputfolder,./output/out_33][timeslice,1.0][timestart,33.0][timestop,34.0] main.mac > ./log/log_33.txt &
+Gate -a [outputfolder,./output/out_34][timeslice,1.0][timestart,34.0][timestop,35.0] main.mac > ./log/log_34.txt &
+Gate -a [outputfolder,./output/out_35][timeslice,1.0][timestart,35.0][timestop,36.0] main.mac > ./log/log_35.txt &
+Gate -a [outputfolder,./output/out_36][timeslice,1.0][timestart,36.0][timestop,37.0] main.mac > ./log/log_36.txt &
+Gate -a [outputfolder,./output/out_37][timeslice,1.0][timestart,37.0][timestop,38.0] main.mac > ./log/log_37.txt &
+Gate -a [outputfolder,./output/out_38][timeslice,1.0][timestart,38.0][timestop,39.0] main.mac > ./log/log_38.txt &
+Gate -a [outputfolder,./output/out_39][timeslice,1.0][timestart,39.0][timestop,40.0] main.mac > ./log/log_39.txt &
+wait
+mkdir ./output/out_40 &
+mkdir ./output/out_41 &
+mkdir ./output/out_42 &
+mkdir ./output/out_43 &
+mkdir ./output/out_44 &
+mkdir ./output/out_45 &
+mkdir ./output/out_46 &
+mkdir ./output/out_47 &
+wait
+Gate -a [outputfolder,./output/out_40][timeslice,1.0][timestart,40.0][timestop,41.0] main.mac > ./log/log_40.txt &
+Gate -a [outputfolder,./output/out_41][timeslice,1.0][timestart,41.0][timestop,42.0] main.mac > ./log/log_41.txt &
+Gate -a [outputfolder,./output/out_42][timeslice,1.0][timestart,42.0][timestop,43.0] main.mac > ./log/log_42.txt &
+Gate -a [outputfolder,./output/out_43][timeslice,1.0][timestart,43.0][timestop,44.0] main.mac > ./log/log_43.txt &
+Gate -a [outputfolder,./output/out_44][timeslice,1.0][timestart,44.0][timestop,45.0] main.mac > ./log/log_44.txt &
+Gate -a [outputfolder,./output/out_45][timeslice,1.0][timestart,45.0][timestop,46.0] main.mac > ./log/log_45.txt &
+Gate -a [outputfolder,./output/out_46][timeslice,1.0][timestart,46.0][timestop,47.0] main.mac > ./log/log_46.txt &
+Gate -a [outputfolder,./output/out_47][timeslice,1.0][timestart,47.0][timestop,48.0] main.mac > ./log/log_47.txt &
+wait
+mkdir ./output/out_48 &
+mkdir ./output/out_49 &
+mkdir ./output/out_50 &
+mkdir ./output/out_51 &
+mkdir ./output/out_52 &
+mkdir ./output/out_53 &
+mkdir ./output/out_54 &
+mkdir ./output/out_55 &
+wait
+Gate -a [outputfolder,./output/out_48][timeslice,1.0][timestart,48.0][timestop,49.0] main.mac > ./log/log_48.txt &
+Gate -a [outputfolder,./output/out_49][timeslice,1.0][timestart,49.0][timestop,50.0] main.mac > ./log/log_49.txt &
+Gate -a [outputfolder,./output/out_50][timeslice,1.0][timestart,50.0][timestop,51.0] main.mac > ./log/log_50.txt &
+Gate -a [outputfolder,./output/out_51][timeslice,1.0][timestart,51.0][timestop,52.0] main.mac > ./log/log_51.txt &
+Gate -a [outputfolder,./output/out_52][timeslice,1.0][timestart,52.0][timestop,53.0] main.mac > ./log/log_52.txt &
+Gate -a [outputfolder,./output/out_53][timeslice,1.0][timestart,53.0][timestop,54.0] main.mac > ./log/log_53.txt &
+Gate -a [outputfolder,./output/out_54][timeslice,1.0][timestart,54.0][timestop,55.0] main.mac > ./log/log_54.txt &
+Gate -a [outputfolder,./output/out_55][timeslice,1.0][timestart,55.0][timestop,56.0] main.mac > ./log/log_55.txt &
+wait
+mkdir ./output/out_56 &
+mkdir ./output/out_57 &
+mkdir ./output/out_58 &
+mkdir ./output/out_59 &
+mkdir ./output/out_60 &
+mkdir ./output/out_61 &
+mkdir ./output/out_62 &
+mkdir ./output/out_63 &
+wait
+Gate -a [outputfolder,./output/out_56][timeslice,1.0][timestart,56.0][timestop,57.0] main.mac > ./log/log_56.txt &
+Gate -a [outputfolder,./output/out_57][timeslice,1.0][timestart,57.0][timestop,58.0] main.mac > ./log/log_57.txt &
+Gate -a [outputfolder,./output/out_58][timeslice,1.0][timestart,58.0][timestop,59.0] main.mac > ./log/log_58.txt &
+Gate -a [outputfolder,./output/out_59][timeslice,1.0][timestart,59.0][timestop,60.0] main.mac > ./log/log_59.txt &
+Gate -a [outputfolder,./output/out_60][timeslice,1.0][timestart,60.0][timestop,61.0] main.mac > ./log/log_60.txt &
+Gate -a [outputfolder,./output/out_61][timeslice,1.0][timestart,61.0][timestop,62.0] main.mac > ./log/log_61.txt &
+Gate -a [outputfolder,./output/out_62][timeslice,1.0][timestart,62.0][timestop,63.0] main.mac > ./log/log_62.txt &
+Gate -a [outputfolder,./output/out_63][timeslice,1.0][timestart,63.0][timestop,64.0] main.mac > ./log/log_63.txt &
+wait
+mkdir ./output/out_64 &
+mkdir ./output/out_65 &
+mkdir ./output/out_66 &
+mkdir ./output/out_67 &
+mkdir ./output/out_68 &
+mkdir ./output/out_69 &
+mkdir ./output/out_70 &
+mkdir ./output/out_71 &
+wait
+Gate -a [outputfolder,./output/out_64][timeslice,1.0][timestart,64.0][timestop,65.0] main.mac > ./log/log_64.txt &
+Gate -a [outputfolder,./output/out_65][timeslice,1.0][timestart,65.0][timestop,66.0] main.mac > ./log/log_65.txt &
+Gate -a [outputfolder,./output/out_66][timeslice,1.0][timestart,66.0][timestop,67.0] main.mac > ./log/log_66.txt &
+Gate -a [outputfolder,./output/out_67][timeslice,1.0][timestart,67.0][timestop,68.0] main.mac > ./log/log_67.txt &
+Gate -a [outputfolder,./output/out_68][timeslice,1.0][timestart,68.0][timestop,69.0] main.mac > ./log/log_68.txt &
+Gate -a [outputfolder,./output/out_69][timeslice,1.0][timestart,69.0][timestop,70.0] main.mac > ./log/log_69.txt &
+Gate -a [outputfolder,./output/out_70][timeslice,1.0][timestart,70.0][timestop,71.0] main.mac > ./log/log_70.txt &
+Gate -a [outputfolder,./output/out_71][timeslice,1.0][timestart,71.0][timestop,72.0] main.mac > ./log/log_71.txt &
+wait
+mkdir ./output/out_72 &
+mkdir ./output/out_73 &
+mkdir ./output/out_74 &
+wait
+Gate -a [outputfolder,./output/out_72][timeslice,1.0][timestart,72.0][timestop,73.0] main.mac > ./log/log_72.txt &
+Gate -a [outputfolder,./output/out_73][timeslice,1.0][timestart,73.0][timestop,74.0] main.mac > ./log/log_73.txt &
+Gate -a [outputfolder,./output/out_74][timeslice,1.0][timestart,74.0][timestop,75.0] main.mac > ./log/log_74.txt &
+wait
+)
+) &
+htop
